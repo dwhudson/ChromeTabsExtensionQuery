62a6b9415970eafa09b2d3e068531ad5357693b6
Dan Hudson <dan.hudson@mocapay.com>
2016-04-01 21:50:03 -0600

    Shows chrome .url ouput in alert

--- background.js
+++ background.js
@@ -17,10 +17,43 @@ console.log('Booya');
     //console.log('inside callback function');
     //console.log('DATA.URL: ',data);
 //}
-
+/*
 chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      console.log(tabs[0]);
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (array_of_tabs) {
+        var tab = array_of_tabs[0];
+        var url = tab.url;
+        console.log('url: ', url);
     });
 });
+*/
+/*
+chrome.tabs.query({ active: true, currentWindow: true}, 
+    function (tabs) {
+        console.log(tabs[0]);
+    }
+);
+*/
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.query({'active': true, currentWindow: true},
+       function(tabs){
+          alert(tabs[0].url);
+       }
+    );
+});
+/*
+var forumTabs = new Array();
 
+chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
+   for(var i = 0; i < tabs.length; i++) {
+    forumTabs[i] = tabs[i];
+   }
+   
+   for(var i = 0; i < forumTabs.length; i++) {
+       if(forumTabs[i] !== null) {
+           window.console.log(forumTabs[i].url);
+       } else {
+           window.console.log("??" + i);
+       }
+   }
+});
+*/
